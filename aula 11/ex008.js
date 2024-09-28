12d6d4d16e018a6c204ead90609574e960ac257b
Vinny997 <st-vinicius14@outlook.com>
2024-09-28 18:30:58 -0300

    primeira aula de condições

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60) { // CONDIÇÃO SIMPLES !!!!! ----------------------------------------
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
