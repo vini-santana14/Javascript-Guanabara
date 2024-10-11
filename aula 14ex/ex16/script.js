0a57284bc8b2299f27d06b2cc2ea8299ea05715b
vini-sant14 <st-vinicius14@outlook.com>
2024-10-11 18:13:21 -0300

    update

--- aula 14ex/ex16/script.js
+++ aula 14ex/ex16/script.js
@@ -1,1 +1,11 @@
-alert("olá")
+var inicio = document.getElementById('iinicio')
+var fim = document.getElementById('ifim')
+var passo = document.getElementById('ipasso')
+
+function contar () {
+    var s = inicio + passo
+    while (s <= fim) {
+        console.log(s)
+        s + passo
+    }
+}
