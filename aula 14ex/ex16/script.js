de6b1b07f7cb91e6dbb3cd896d7bfc68866784ee
Vinny997 <st-vinicius14@outlook.com>
2024-10-14 13:34:13 -0300

    exercicio de contagem finished

--- aula 14ex/ex16/script.js
+++ aula 14ex/ex16/script.js
@@ -1,11 +1,36 @@
-var inicio = document.getElementById('iinicio')
-var fim = document.getElementById('ifim')
-var passo = document.getElementById('ipasso')
+function contar() {
+    let ini = document.getElementById('txti') 
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-function contar () {
-    var s = inicio + passo
-    while (s <= fim) {
-        console.log(s)
-        s + passo
+        if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+            res.innerHTML = 'Impossível contar!'
+           // window.alert('[ERRO] Faltam dados!')
+            
+        } else {
+            res.innerHTML = 'Contando: <br>'
+            let i = Number(ini.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+            if( p <= 0) {
+                window.alert('Passo inválido! Considerando PASSO 1')
+                p = 1
+            }
+            
+            if ( i < f) {
+                //contagem crescente
+                for(let c = i; c <= f; c += p) {
+                    res.innerHTML += ` ${c} \u{1F449}`
+                }
+                res.innerHTML += `\u{1F3C1}`
+        } else {
+
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
